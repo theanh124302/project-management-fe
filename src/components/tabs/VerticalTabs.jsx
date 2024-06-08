0f2f2b3251bcf12b34d65b541c03eea3c67729ec
Anh Hoang The <hoangtheanhc2lp@gmail.com>
2024-06-08 16:14:07 +0700

    daily

--- src/components/tabs/VerticalTabs.jsx
+++ src/components/tabs/VerticalTabs.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
-import { FaArrowLeft, FaTasks, FaInfoCircle, FaDatabase, FaBug, FaTachometerAlt, FaCalendarAlt } from 'react-icons/fa';
+import { FaArrowLeft, FaTasks, FaInfoCircle, FaDatabase, FaBug, FaTachometerAlt, FaCalendarAlt, FaFileAlt } from 'react-icons/fa';
 import '../../public/css/VerticalTabs.css';  // Import CSS
 
 const VerticalTabs = () => {
@@ -14,11 +14,15 @@ const VerticalTabs = () => {
     // Update selected tab based on current location
     if (location.pathname.includes('/dashboard')) setSelectedTab(0);
     else if (location.pathname.includes('/schedule')) setSelectedTab(1);
-    else if (location.pathname.includes('/api')) setSelectedTab(2);
-    else if (location.pathname.includes('/task')) setSelectedTab(3);
-    else if (location.pathname.includes('/database')) setSelectedTab(4);
-    else if (location.pathname.includes('/issue')) setSelectedTab(5);
-    else if (location.pathname.includes('/detail')) setSelectedTab(6);
+    else if (location.pathname.includes('/daily-report')) setSelectedTab(2);
+    else if (location.pathname.includes('/api')) setSelectedTab(3);
+    else if (location.pathname.includes('/task')) setSelectedTab(4);
+    else if (location.pathname.includes('/database')) setSelectedTab(5);
+    else if (location.pathname.includes('/issue')) setSelectedTab(6);
+    else if (location.pathname.includes('/file')) setSelectedTab(7);
+    else if (location.pathname.includes('/detail')) setSelectedTab(8);
+
+
   }, [location.pathname]);
 
   const handleTabChange = (tabIndex, path) => {
@@ -53,45 +57,54 @@ const VerticalTabs = () => {
         <a
           className={`nav-link text-white ${selectedTab === 2 ? 'active' : ''}`}
           href="#"
-          onClick={() => handleTabChange(2, `/project/${projectId}/api`)}
+          onClick={() => handleTabChange(2, `/project/${projectId}/daily-report`)}
         >
-          <FaTasks className="mr-2" /> API
+          <FaFileAlt className="mr-2" /> Daily Report
         </a>
         <a
           className={`nav-link text-white ${selectedTab === 3 ? 'active' : ''}`}
           href="#"
-          onClick={() => handleTabChange(3, `/project/${projectId}/task`)}
+          onClick={() => handleTabChange(3, `/project/${projectId}/api`)}
         >
-          <FaTasks className="mr-2" /> Task
+          <FaTasks className="mr-2" /> API
         </a>
         <a
           className={`nav-link text-white ${selectedTab === 4 ? 'active' : ''}`}
           href="#"
-          onClick={() => handleTabChange(4, `/project/${projectId}/database`)}
+          onClick={() => handleTabChange(4, `/project/${projectId}/task`)}
         >
-          <FaDatabase className="mr-2" /> Database
+          <FaTasks className="mr-2" /> Task
         </a>
         <a
           className={`nav-link text-white ${selectedTab === 5 ? 'active' : ''}`}
           href="#"
-          onClick={() => handleTabChange(5, `/project/${projectId}/issue`)}
+          onClick={() => handleTabChange(5, `/project/${projectId}/database`)}
+        >
+          <FaDatabase className="mr-2" /> Database
+        </a>
+        <a
+          className={`nav-link text-white ${selectedTab === 6 ? 'active' : ''}`}
+          href="#"
+          onClick={() => handleTabChange(6, `/project/${projectId}/issue`)}
         >
           <FaBug className="mr-2" /> Issue
         </a>
         <a
-          className={`nav-link text-white ${selectedTab === 6 ? 'active' : ''}`}
+          className={`nav-link text-white ${selectedTab === 7 ? 'active' : ''}`}
           href="#"
-          onClick={() => handleTabChange(6, `/project/${projectId}/detail`)}
-        >
-          <FaInfoCircle className="mr-2" /> Detail
-        </a>
-        <a
-          className={`nav-link text-white ${selectedTab === 6 ? 'active' : ''}`}
-          href="#"
-          onClick={() => handleTabChange(6, `/project/${projectId}/file`)}
+          onClick={() => handleTabChange(7, `/project/${projectId}/file`)}
         >
           <FaInfoCircle className="mr-2" /> File
         </a>
+        <a
+          className={`nav-link text-white ${selectedTab === 8 ? 'active' : ''}`}
+          href="#"
+          onClick={() => handleTabChange(8, `/project/${projectId}/detail`)}
+        >
+          <FaInfoCircle className="mr-2" /> Detail
+        </a>
+
+
       </nav>
     </div>
   );
